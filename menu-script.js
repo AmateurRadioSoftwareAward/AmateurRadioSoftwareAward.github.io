c20d646de7a4cb81b7fbfc8ee51d82ad8b08923e
Claus Niesen <cniesen@users.noreply.github.com>
2020-08-14 19:25:41 -0500

    Updates

--- menu-script.js
+++ menu-script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   var section = location.pathname.split("/")[1];
+
   if (section === "") {
       section = "home"
   }
+  
+  // remove .html and .htm extensions
+  var extensionPosition = section.lastIndexOf("htm");
+  if (extensionPosition > 0) {
+	  section = section.substring(0, extensionPosition - 1);
+  }
+  
   $(".menu-button").removeClass("active");
   $(".menu-button[data-menu=" + section + "]").addClass("active");
 
